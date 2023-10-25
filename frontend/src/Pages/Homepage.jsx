841a10d2668f3f4a61cdd2163bfee53edaebe339
Musyaffaq <Musyaffaq@users.noreply.github.com>
2023-10-25 16:50:28 +0800

    Add create squad page

--- frontend/src/Pages/Homepage.jsx
+++ frontend/src/Pages/Homepage.jsx
@@ -4,21 +4,22 @@ import HomepageStatus from "../Components/HomepageStatus";
 import { Button } from "@mui/material";
 import DisplayGroupCards from "../Components/DisplayGroupCards";
 import DisplayEventCards from "../Components/DisplayEventCards";
+import { Link } from "react-router-dom";
 
 function Homepage() {
   return (
-    <>
-      <Grid container spacing={2}>
-        <Grid item xs={10}>
-          <HomepageStatus />
-        </Grid>
-        <Grid item xs={2}>
-          <Button variant="contained">Create a Squad</Button>
-        </Grid>
-        <DisplayGroupCards />
-        <DisplayEventCards />
+    <Grid container spacing={2}>
+      <Grid item xs={10}>
+        <HomepageStatus />
       </Grid>
-    </>
+      <Grid item xs={2}>
+        <Link to="/squads/create">
+          <Button variant="contained">Create a Squad</Button>
+        </Link>
+      </Grid>
+      <DisplayGroupCards />
+      <DisplayEventCards />
+    </Grid>
   );
 }
 
