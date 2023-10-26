61d80dc7cbcf8f4b70275b17b4efbdfae9e4a425
Benhzy <107015176+Benhzy@users.noreply.github.com>
2023-10-26 08:31:15 +0800

    Modified GroupCard and DisplayGroupCards
    Added the pngs for users and group card design

--- frontend/src/Components/DisplayGroupCards.jsx
+++ frontend/src/Components/DisplayGroupCards.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import {
+  Box,
   Card,
   CardActionArea,
   CardContent,
@@ -8,24 +10,61 @@ import {
 } from "@mui/material";
 import GroupCard from "../Components/GroupCard";
 
+
+const groupDataList = [
+  {
+    name: 'Code Genius',
+    hackathon: 'Code for Good',
+    leader: 'person1.jpg',
+    member1: 'person2.jpg',
+    member2: 'person3.jpg',
+    member3: 'person4.jpg',
+  },
+
+  {
+    name: 'Stark Industries',
+    hackathon: 'SG-IN Hackathon',
+    leader: 'person7.jpg',
+    member1: 'person8.jpg',
+    member2: 'person9.jpg',
+    member3: 'person1.jpg',
+  },
+];
+
+
 function DisplayGroupCards() {
+  const cardActionAreaStyle = {
+    height: '180px',
+    width: '90px',
+    alignItems: 'center',
+    justifyContent: 'center',
+  };
+  const buttonStyle = {
+    height: '143px',
+    width: '60px',
+    alignItems: 'center',
+    justifyContent: 'center',
+  };
   return (
-    <>
-      <GroupCard />
-      <GroupCard />
-      <GroupCard />
-      <GroupCard />
-      <GroupCard />
-      <Grid item xs={2}>
-        <CardActionArea>
-          <Card>
-            <CardContent>
-              <Typography>See More</Typography>
+    <div  className="display-event-card">
+      <>
+      {groupDataList.map((groupData, index) => (
+        <GroupCard key={index} groupData={groupData} />
+      ))}
+      </>
+      <Grid item xs={2} >
+        <CardActionArea style = {cardActionAreaStyle} >
+          <Card style = {cardActionAreaStyle}>
+            <CardContent justifyContent="center">
+              <Box display="flex" style = {buttonStyle}>
+                <ArrowForwardIosIcon fontSize="large" />
+              </Box>
             </CardContent>
           </Card>
         </CardActionArea>
       </Grid>
-    </>
+    </div>
+
   );
 }
 
--- frontend/src/Components/GroupCard.jsx
+++ frontend/src/Components/GroupCard.jsx
@@ -3,24 +3,59 @@ import {
   CardContent,
   Grid,
   Typography,
-  CardActions,
+  CardActionArea,
   Button,
 } from "@mui/material";
 import React from "react";
 
-function GroupCard() {
+const circleContainer = {
+  width: '55px', 
+  height: '55px',
+  alignItems: 'center',
+  overflow: 'hidden',
+  display: 'flex',
+  borderRadius: '50%',
+};
+
+const imageRowContainer = {
+  display: 'flex', // Use flexbox to arrange items in a row
+  alignItems: 'center', // Align items vertically in the center
+  justifyContent: 'space-around', // Adjust as needed to control spacing
+};
+
+const cardStyles = {
+  width: '291px', 
+  alignItems: 'center',
+  justifyContent: 'space-around',
+  flexwrap: 'wrap',
+};
+
+const nameStyle = {
+  marginBottom: '10px', // Add space below the name
+};
+
+function GroupCard( {groupData} ) {
   return (
-    <Grid item xs={2}>
-      <Card>
+    <Grid item xs={4.662}>
+      <CardActionArea style = {cardStyles}>
+        <card>
         <CardContent>
-          <Typography>Group 1</Typography>
-          <Typography>Information</Typography>
-          <Typography>More Information</Typography>
+          <Typography variant="h5" style={nameStyle}>
+            {groupData.name}
+          </Typography>
+          <Typography variant="h6" style={nameStyle}>
+            {groupData.hackathon}
+          </Typography>
+          <div style={imageRowContainer}>
+            <img src={groupData.leader} style={circleContainer} alt="Leader" />
+            <img src={groupData.member1} style={circleContainer} alt="member1" />
+            <img src={groupData.member2} style={circleContainer} alt="member2" />
+            <img src={groupData.member3} style={circleContainer} alt="member3" />
+          </div>
         </CardContent>
-        <CardActions>
-          <Button size="small">See More</Button>
-        </CardActions>
-      </Card>
+        </card>
+      </CardActionArea>
+      
     </Grid>
   );
 }
