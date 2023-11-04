89130906f993e0132fa8d6e37c8066e46ff8d839
Benhzy <107015176+Benhzy@users.noreply.github.com>
2023-11-04 18:24:36 +0800

    DId stuff to the squad view stuff

--- frontend/src/Components/SquadViewComponent.jsx
+++ frontend/src/Components/SquadViewComponent.jsx
@@ -23,14 +23,47 @@ const inputArr = [
 
 
 const SquadViewComponent = () => {
+<<<<<<< Updated upstream
   const { squadName, EventName, SkillsRequired, FromDate, ToDate, TimeCommitment, Personality, ConfirmedMembers } = inputArr[0];
+=======
+  const { squadid } = useParams(); // this is the username
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/get-squad/${squadid}`)
+      .then((res) => res.json()) // Parse the response as JSON
+      .then((data) => {
+        setSquadName(data.squad.squadName);
+        setEventName(data.squad.eventName);
+        setSkillsRequired(data.squad.skillsRequired);
+        setFromDate(data.squad.fromDate.slice(5, 16));
+        setToDate(data.squad.toDate.slice(5, 16));
+        setTimeCommitment(data.squad.timeCommitment);
+        setPersonality(data.squad.personality);
+        setConfirmedMembers(data.squad.confirmedMembers);
+      });
+  }, []);
+
+  const [squadName, setSquadName] = useState("");
+  const [eventName, setEventName] = useState("");
+  const [skillsRequired, setSkillsRequired] = useState([]);
+  const [fromDate, setFromDate] = useState("");
+  const [toDate, setToDate] = useState("");
+  const [timeCommitment, setTimeCommitment] = useState(0);
+  const [personality, setPersonality] = useState("");
+  const [confirmedMembers, setConfirmedMembers] = useState([]);
+
+>>>>>>> Stashed changes
   return (
     <div className="group-view">
       <h1>{squadName}</h1>
       <h2>{EventName}</h2>
 
+<<<<<<< Updated upstream
     <ImageCard />
       
+=======
+      <ImageCard Event = {eventName} />
+>>>>>>> Stashed changes
 
       <div className="group-info">
         <h2>Group Information</h2>
